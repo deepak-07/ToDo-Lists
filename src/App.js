021d8c548a74a647e7f913c9150e3a2d75291c7f
Deepak Yadav <42115559+deepak-07@users.noreply.github.com>
2021-02-05 23:29:02 +0530

    Heading and input added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./styles.css";
-
+import React, { useState } from "react";
 export default function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  function inputHandler(event) {
+    var setTask = event.target.value;
+    setTask(setTask);
+  }
+  function addTask() {}
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>To-Do </h1>
+
+      {/* <h2>Start editing to see some magic happen!</h2> */}
+
+      <input
+        type="text"
+        placeHolder="Enter your task"
+        onChange={inputHandler}
+      />
+      <button onClick={addTask}> + </button>
     </div>
   );
 }
