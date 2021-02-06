c0035ba8150c993f7099ac3381df7bed9ff12ab0
Deepak Yadav <42115559+deepak-07@users.noreply.github.com>
2021-02-07 00:23:00 +0530

    feat: Done and Delete button added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./styles.css";
 import React, { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheckCircle, faTrash);
 export default function App() {
   const [task, setTask] = useState("");
   const [list, setList] = useState([]);
+  const [done, setDone] = useState([]);
 
   function inputHandler(event) {
     setTask(event.target.value);
   }
+
   function addTask() {
     setList((oldValue) => [...oldValue, task]);
     setTask(" ");
-    console.log(task, list);
+    console.log(task, list, done);
+  }
+  function delTask(item) {
+    setList((oldValue) => {
+      return oldValue.filter((element, index) => {
+        return index !== item;
+      });
+    });
+  }
+
+  function checkDone(itemVal, item) {
+    setDone((oldValue) => [...oldValue, itemVal]);
+    delTask(item);
+    console.log("Deleted");
   }
   return (
     <div className="App">
@@ -26,8 +46,28 @@ export default function App() {
       />
       <button onClick={addTask}> + </button>
 
-      {list.map((item) => {
-        return <li key={item}> {item} </li>;
+      {list.map((itemVal, item) => {
+        return (
+          <div className="taskName">
+            <li key={item}>
+              <FontAwesomeIcon icon="trash" onClick={() => delTask(item)} />
+              {itemVal}
+              <FontAwesomeIcon
+                icon="check-circle"
+                onClick={() => checkDone(itemVal, item)}
+              />
+            </li>
+          </div>
+        );
+      })}
+      {done.map((items) => {
+        return (
+          <div className="taskDone">
+            <li key={items} style={{ textDecorationLine: "line-through" }}>
+              {items}
+            </li>
+          </div>
+        );
       })}
     </div>
   );
