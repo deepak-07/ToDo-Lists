cc1bc428e66be883d17183c1c31f22cca065401b
Deepak Yadav <42115559+deepak-07@users.noreply.github.com>
2021-02-11 01:36:05 +0530

    fix: removed completed task after saving

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ export default function App() {
         tname: taskHead
       }));
       setTaskHead("");
+      setDone([]);
     } else alert("Please enter sub heading");
     if (list !== "") {
       setCategory((category) => ({
